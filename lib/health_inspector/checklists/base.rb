7e08701e4e8fd6f09345141e26ae65d8f42708ac
Adam Sinnett <sinneta@webtrends.com>
2012-09-25 12:15:09 -0700

    Pretty printed diff of hashed environment files and roles. Added diff to roles

--- lib/health_inspector/checklists/base.rb
+++ lib/health_inspector/checklists/base.rb
@@ -69,7 +69,7 @@ module HealthInspector
               if original[key].kind_of?(Hash) && other[key].kind_of?(Hash)
                 memo[key] = diff(original[key], other[key])
               else
-                memo[key] = [original[key], other[key]]
+                memo[key] = {"server" => original[key], "local" => other[key] }
               end
             end
             memo
@@ -93,15 +93,46 @@ module HealthInspector
         puts color('bright pass', "✓") + " #{subject}"
       end
 
+      def print_failures_from_hash(message, depth=0)
+        message.keys.each do |key|
+          print_key(key,depth)
+          if message[key].include? "server"
+            print_value_diff(message[key],depth)
+            message[key].delete_if {|k,v| k=="server"||"local"}
+            print_failures_from_hash(message[key],depth+1) if !message[key].empty?
+          else
+            print_failures_from_hash(message[key], depth+1)
+          end
+        end
+      end
+
       def print_failures(subject, failures)
         puts color('bright fail', "- #{subject}")
 
         failures.each do |message|
-          puts color('bright yellow', "  #{message}")
+          if message.kind_of?(Hash)
+            puts color('bright yellow',"  has the following values mismatched on the server and repo\n")
+            print_failures_from_hash(message)
+          else
+            puts color('bright yellow', "  #{message}")
+          end
         end
       end
 
-      def load_ruby_or_json_from_local(chef_class, folder, name)
+      def print_key(key,depth)
+        puts (color('bright yellow',"#{key} => ")).prepend(" "*(4*depth+2))
+      end
+
+      def print_value_diff(value,depth)
+          print (color('bright fail',"Server Value: ")).prepend(" "*(4*depth+4))
+          print value["server"]
+          print "\n"
+          print (color('bright fail',"Local Value:  ")).prepend(" "*(4*depth+4))
+          print value["local"] 
+          print "\n\n"
+      end
+
+     def load_ruby_or_json_from_local(chef_class, folder, name)
         path_template = "#{@context.repo_path}/#{folder}/#{name}.%s"
         ruby_pathname = Pathname.new(path_template % "rb")
         json_pathname = Pathname.new(path_template % "json")
@@ -111,9 +142,9 @@ module HealthInspector
           instance = chef_class.new
           instance.from_file(ruby_pathname.to_s)
         elsif json_pathname.exist?
-          instance = chef_class.json_create( JSON.parse( json_pathname.read ) )
+          instance = chef_class.json_create( Yajl::Parser.parse( json_pathname.read ) )
         elsif js_pathname.exist?
-          instance = chef_class.json_create( JSON.parse( js_pathname.read ) )
+          instance = chef_class.json_create( Yajl::Parser.parse( js_pathname.read ) )
         end
 
         instance ? instance.to_hash : nil
